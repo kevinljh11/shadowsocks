f63e0289517f83a563dfa6a58987a808a7363550
kevinljh11 <kevinljh1@hotmail.com>
2017-08-06 07:47:53 +0800

    Update jelkcpsspy.sh

--- jelkcpsspy.sh
+++ jelkcpsspy.sh
@@ -1,1 +1,4 @@
-aaaa
+wget https://github.com/shadowsocks/shadowsocks/archive/master.zip
+wget https://github.com/kevinljh11/shadowsocks/raw/rm/server_linux_amd64
+unzip master.zip
+chmod +x server_linux_amd64
