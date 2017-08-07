7537fe5ced4ddd243df02bf750db83bd54ef79fc
kevinljh11 <kevinljh1@hotmail.com>
2017-08-07 22:13:11 +0800

    Create kcpss.sh

---
+++ kcpss.sh
@@ -0,0 +1,4 @@
+killall kcpserver
+killall ssserver
+./kcpserver -t "0.0.0.0:11003" -l ":11004" -key test -mtu 1350 -sndwnd 2048 -rcvwnd 2048 -crypt none -mode fast2 -dscp 0 -datashard 10 -parityshard 3 -nocomp> kcptun.log 2>&1 &
+nohup /opt/jelastic-python27/bin/ssserver -c /opt/repo/ROOT/ss.json > /dev/null 2>&1 &
