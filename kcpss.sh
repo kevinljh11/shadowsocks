e1e72034bfbda23d3a799b637a94152d55e01b9c
kevinljh11 <kevinljh1@hotmail.com>
2017-08-23 12:16:17 +0800

    Update kcpss.sh

--- kcpss.sh
+++ kcpss.sh
@@ -1,4 +1,6 @@
 killall server_linux_amd64
 killall ssserver
+killall python
 ./server_linux_amd64 -t "0.0.0.0:11003" -l ":11004" -key test -mtu 1350 -sndwnd 2048 -rcvwnd 2048 -crypt none -mode fast2 -dscp 0 -datashard 10 -parityshard 3 -nocomp> kcptun.log 2>&1 &
 nohup /opt/jelastic-python27/bin/ssserver -c /opt/repo/ROOT/ss.json > /dev/null 2>&1 &
+nohup python index.py > /dev/null 2>&1 &
